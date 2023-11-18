834711db9f8df48659d5643b770e16e210f566a8
Paawan Sharma <96773581+PaawanSofTech@users.noreply.github.com>
2023-11-18 14:09:47 +0530

    Add files via upload

---
+++ Cms/articles/urls.py
@@ -0,0 +1,26 @@
+"""djangonautic URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+app_name = 'articles'
+
+urlpatterns = [
+    path('', views.article_list, name = 'list'),
+    path('create', views.article_create, name = 'create'),
+    path('<slug>/', views.article_detail, name = 'detail'),
+]
